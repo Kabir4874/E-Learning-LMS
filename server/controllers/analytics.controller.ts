dd69a030f32d225ed2d2f2c2ef818016cc42f6f8
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-05-29 20:44:09 +0600

    all anylytics done

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -1,4 +1,6 @@
 import { CatchAsyncError } from "../middlewares/catchAsyncErrors";
+import courseModel from "../models/course.model";
+import orderModel from "../models/order.model";
 import userModel from "../models/user.model";
 import { generateLast12MonthsData } from "../utils/analytics.generator";
 import TryCatch from "../utils/tryCatch";
@@ -9,6 +11,26 @@ export const getUserAnalytics = CatchAsyncError(
     res.status(200).json({
       success: true,
       users,
+    });
+  })
+);
+
+export const getCourseAnalytics = CatchAsyncError(
+  TryCatch(async (req, res, next) => {
+    const courses = await generateLast12MonthsData(courseModel);
+    res.status(200).json({
+      success: true,
+      courses,
+    });
+  })
+);
+
+export const getOrderAnalytics = CatchAsyncError(
+  TryCatch(async (req, res, next) => {
+    const orders = await generateLast12MonthsData(orderModel);
+    res.status(200).json({
+      success: true,
+      orders,
     });
   })
 );
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,9 +1,25 @@
 import express from "express";
 
-import { getUserAnalytics } from "../controllers/analytics.controller";
+import {
+  getCourseAnalytics,
+  getOrderAnalytics,
+  getUserAnalytics,
+} from "../controllers/analytics.controller";
 import { authorizeRoles, isAuthenticated } from "../middlewares/auth";
 const router = express.Router();
 
 router.get("/user", isAuthenticated, authorizeRoles("admin"), getUserAnalytics);
+router.get(
+  "/course",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getCourseAnalytics
+);
+router.get(
+  "/order",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getOrderAnalytics
+);
 
 export default router;
