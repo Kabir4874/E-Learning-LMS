bab7a7b8c795fe8fbc7f97db95dbb818fe3503cc
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-06-02 20:53:03 +0600

    loader done

---
+++ client/app/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import "./loader.css";
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
