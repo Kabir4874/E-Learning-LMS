16517fa448d903167d5725b1b22eb58e6c14f6d2
Kabir Ahmed Ridoy <105164822+Kabir4874@users.noreply.github.com>
2025-06-08 15:02:06 +0600

    Update style.ts

--- client/app/styles/style.ts
+++ client/app/styles/style.ts
@@ -6,6 +6,7 @@ export const styles = {
 
   input:
     "w-full text-black! dark:text-white! bg-transparent border! rounded! h-[40px] px-2! outline-none! mt-[10px]! font-poppins",
+
   button:
     "flex justify-center items-center py-3! px-6! rounded-full! cursor-pointer bg-[#2190ff]! min-h-[45px]! w-full font-poppins! font-semibold!",
 };
