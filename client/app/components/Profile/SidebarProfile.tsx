c97df03363471a511c14a7fb19d5c6a0f2df7ddc
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-06-09 13:23:31 +0600

    logout done

--- client/app/components/Profile/SidebarProfile.tsx
+++ client/app/components/Profile/SidebarProfile.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { AiOutlineLogout } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { SiCoursera } from "react-icons/si";
 import Avatar from "../../../public/avatar.png";
 type Props = {
   user: any;
@@ -19,7 +22,7 @@ const SidebarProfile = ({
     <div className="w-full">
       <div
         className={`w-full flex items-center px-3! py-4! cursor-pointer ${
-          active === 1 ? "bg-slate-800!" : "bg-transparent"
+          active === 1 ? "dark:bg-slate-800! bg-white!" : "bg-transparent!"
         }`}
         onClick={() => setActive(1)}
       >
@@ -28,6 +31,45 @@ const SidebarProfile = ({
           alt="avatar"
           className="w-[20px] h-[20px] 800px:w-[30px] 800px:h-[30px] cursor-pointer rounded-full"
         />
+        <h5 className="pl-2! 800px:block hidden font-poppins! dark:text-white! text-black!">
+          My Account
+        </h5>
+      </div>
+      <div
+        className={`w-full flex items-center px-3! py-4! cursor-pointer ${
+          active === 2 ? "dark:bg-slate-800! bg-white!" : "bg-transparent!"
+        }`}
+        onClick={() => setActive(2)}
+      >
+        <RiLockPasswordLine
+          size={20}
+          className="dark:text-white! text-black!"
+        />
+        <h5 className="pl-2! 800px:block hidden font-poppins! dark:text-white! text-black!">
+          Change Password
+        </h5>
+      </div>
+      <div
+        className={`w-full flex items-center px-3! py-4! cursor-pointer ${
+          active === 3 ? "dark:bg-slate-800! bg-white!" : "bg-transparent!"
+        }`}
+        onClick={() => setActive(3)}
+      >
+        <SiCoursera size={20} className="dark:text-white! text-black!" />
+        <h5 className="pl-2! 800px:block hidden font-poppins! dark:text-white! text-black!">
+          Enrolled Courses
+        </h5>
+      </div>
+      <div
+        className={`w-full flex items-center px-3! py-4! cursor-pointer ${
+          active === 4 ? "dark:bg-slate-800! bg-white!" : "bg-transparent!"
+        }`}
+        onClick={() => logoutHandler()}
+      >
+        <AiOutlineLogout size={20} className="dark:text-white! text-black!" />
+        <h5 className="pl-2! 800px:block hidden font-poppins! dark:text-white! text-black!">
+          Logout
+        </h5>
       </div>
     </div>
   );
